e8a951967a992b6551f427bd95a1b82506345241
Cyber Celt <33097451+Cyb3rC3lt@users.noreply.github.com>
2023-07-18 16:07:33 +0100

    Update resolve.py

--- resolve.py
+++ resolve.py
@@ -7,25 +7,25 @@ import os
 class Lookup:
 
  if (os.path.isfile(sys.argv[1])):
-    with open(sys.argv[1]) as hosts:
-     for host in hosts:
-        host = host.rstrip('\n')
-        cmd = "nmblookup -A " + host + "| sed -n 2p | cut -d ' ' -f1 | xargs"
+    with open(sys.argv[1]) as ips:
+     for ip in ips:
+        ip = ip.rstrip('\n')
+        cmd = "nmblookup -A " + ip + "| sed -n 2p | cut -d ' ' -f1 | xargs"
         nmblookup = subprocess.run(
             ["sh", "-c", cmd],
             text=True,
             stdout=subprocess.PIPE,
             check=True)
-        split = nmblookup.stdout.split('\n')
-        print('{} : {}'.format(host, split[0]))
+        hostname = nmblookup.stdout.split('\n')
+        print('{} : {}'.format(ip, hostname[0]))
 
  else:
-    for host in sys.argv[1:]:
-        cmd = "nmblookup -A " + host + "| sed -n 2p | cut -d ' ' -f1 | xargs"
+    for ip in sys.argv[1:]:
+        cmd = "nmblookup -A " + ip + "| sed -n 2p | cut -d ' ' -f1 | xargs"
         nmblookup = subprocess.run(
             ["sh", "-c", cmd],
             text=True,
             stdout=subprocess.PIPE,
             check=True)
-        split = nmblookup.stdout.split('\n')
-        print('{} : {}'.format(host, split[0]))
+        hostname = nmblookup.stdout.split('\n')
+        print('{} : {}'.format(ip, hostname[0]))
