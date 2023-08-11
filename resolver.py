994cec30314d1f5516edbddbb61280f6f5828629
Cyber Celt <33097451+Cyb3rC3lt@users.noreply.github.com>
2023-08-11 17:09:12 +0100

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -6,32 +6,48 @@ import os
 
 class Resolver:
 
+ # Process a file
  if (os.path.isfile(sys.argv[1])):
     with open(sys.argv[1]) as ips:
      for ip in ips:
         ip = ip.rstrip('\n')
-        cmd = "nslookup " + ip + " | awk '{print $4}' | sed 's/.$//'"
-        nslookup = subprocess.run(
-            ["sh", "-c", cmd],
-            text=True,
-            stdout=subprocess.PIPE,
-            check=True)
-        hostname = nslookup.stdout.split('\n')
+        cmd = "nslookup " + ip
+        try:
+          nslookup = subprocess.run(
+              ["sh", "-c", cmd],
+              text=True,
+              stdout=subprocess.PIPE,
+              check=True)
+        except subprocess.CalledProcessError as e:
+          print('{} : {}'.format(ip, "Unable to resolve this IP"))
+          continue
+        
+        nslookup = nslookup.stdout.split('\n')
+        hostnamesplit = nslookup[0].split('=')
+        hostname = hostnamesplit[1][:len(hostnamesplit[1])-1]
         if ("fin" in hostname):
             print('{} : {}'.format(ip, "Unable to resolve this IP"))
         else:
-            print('{} : {}'.format(ip, hostname[0]))
-     sys.exit()
+            print('{} : {}'.format(ip, hostname))  
+ 
+ # Process single IPs  
  else:
     for ip in sys.argv[1:]:
-        cmd = "nslookup " + ip + " | awk '{print $4}' | sed 's/.$//'"
-        nslookup = subprocess.run(
-            ["sh", "-c", cmd],
-            text=True,
-            stdout=subprocess.PIPE,
-            check=True)
-        hostname = nslookup.stdout.split('\n')
+        cmd = "nslookup " + ip
+        try:
+          nslookup = subprocess.run(
+              ["sh", "-c", cmd],
+              text=True,
+              stdout=subprocess.PIPE,
+              check=True)
+        except subprocess.CalledProcessError as e:
+          print('{} : {}'.format(ip, "Unable to resolve this IP"))
+          continue
+
+        nslookup = nslookup.stdout.split('\n')
+        hostnamesplit = nslookup[0].split('=')
+        hostname = hostnamesplit[1][:len(hostnamesplit[1])-1]
         if ("fin" in hostname):
             print('{} : {}'.format(ip, "Unable to resolve this IP"))
         else:
-            print('{} : {}'.format(ip, hostname[0]))
+            print('{} : {}'.format(ip, hostname))
