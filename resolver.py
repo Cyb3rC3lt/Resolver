107b905e05f9f8bbd20fe5c1f0ca9def91e9ec05
David Kennedy <33097451+Cyb3rC3lt@users.noreply.github.com>
2025-04-04 16:11:57 +0100

    Update resolver.py

--- resolver.py
+++ resolver.py
@@ -1,49 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import subprocess
-import sys
-import os
+import socket
+import argparse
 
-class Resolver:
+def resolve(input_file, output_file):
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        for line in infile:
+            value = line.strip()
+            if value:
+                try:
+                    # Determine if the input is a hostname or an IP
+                    if value.replace(".", "").isdigit():  # Simple check for an IP address
+                        hostname = socket.gethostbyaddr(value)[0]
+                        outfile.write(f"{value},{hostname}\n")
+                    else:
+                        ip = socket.gethostbyname(value)
+                        outfile.write(f"{value},{ip}\n")
+                except (socket.gaierror, socket.herror):
+                    outfile.write(f"{value},Unable to resolve\n")
 
- # Process a file
- if (os.path.isfile(sys.argv[1])):
+def main():
+    parser = argparse.ArgumentParser(description="Resolve hostnames to IP addresses and vice versa.")
+    parser.add_argument("-i", "--input", required=True, help="Input file containing hostnames or IPs.")
+    parser.add_argument("-o", "--output", required=True, help="Output file for resolved results.")
+    args = parser.parse_args()
+    
+    resolve(args.input, args.output)
 
-    with open(sys.argv[1]) as ips:
-     nonblankips = filter(None, (ip1.rstrip() for ip1 in ips))
-     for ip in nonblankips:
-        ip = ip.rstrip('\n')
-        cmd = "nslookup " + ip
-        try:
-          nslookup = subprocess.run(
-              ["sh", "-c", cmd],
-              text=True,
-              stdout=subprocess.PIPE,
-              check=True)
-        except subprocess.CalledProcessError as e:
-          print('{} : {}'.format(ip, "Unable to resolve this IP"))
-          continue
-        
-        nslookup = nslookup.stdout.split('\n')
-        hostnamesplit = nslookup[0].split('=')
-        hostname = hostnamesplit[1][:len(hostnamesplit[1])-1] 
-        print('{} : {}'.format(ip, hostname))  
- 
- # Process single IPs  
- else:
-    for ip in sys.argv[1:]:
-        cmd = "nslookup " + ip
-        try:
-          nslookup = subprocess.run(
-              ["sh", "-c", cmd],
-              text=True,
-              stdout=subprocess.PIPE,
-              check=True)
-        except subprocess.CalledProcessError as e:
-          print('{} : {}'.format(ip, "Unable to resolve this IP"))
-          continue
-
-        nslookup = nslookup.stdout.split('\n')
-        hostnamesplit = nslookup[0].split('=')
-        hostname = hostnamesplit[1][:len(hostnamesplit[1])-1]
-        print('{} : {}'.format(ip, hostname))
+if __name__ == "__main__":
+    main()
